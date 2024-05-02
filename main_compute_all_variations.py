0d124f8e69294c1631096468e3e75e08c852c79c
mrousselet <maxime.rousselet@get.omp.eu>
2024-05-02 07:33:12 +0200

    working on loops

--- main_compute_all_variations.py
+++ main_compute_all_variations.py
@@ -11,23 +11,19 @@ clear_subs()
 
 anelasticity_description_ids, model_filenames = Love_numbers_for_options_for_models_for_parameters(
     elasticity_model_names=["PREM"],
-    long_term_anelasticity_model_names=["Caron", "Lambeck_2017", "Lau_2016", "VM5a", "VM7"],
+    long_term_anelasticity_model_names=["VM7"],
     short_term_anelasticity_model_names=[
-        "Benjamin_Q_PAR3P",
-        "Benjamin_Q_PREM",
-        "Benjamin_Q_QL6",
-        "Benjamin_Q_QM1",
         "Benjamin_Q_Resovsky",
     ],
-    parameters={ModelPart.long_term_anelasticity: {"eta_m": {"D''": [[1e17]], "ASTHENOSPHERE": [[3e19]]}}},
+    parameters={ModelPart.long_term_anelasticity: {"eta_m": {"ASTHENOSPHERE": [[3e19]]}}},
     symlinks=False,
 )
 load_result_folders: list[Path] = load_signal_for_options_for_models_for_parameters_for_elastic_load_signals(
     anelasticity_description_ids=anelasticity_description_ids,
     model_filenames=model_filenames,
     load_signal_hyper_parameter_variations={
-        "case": ["mean", "lower", "upper", "best", "worst"],
-        "little_isostatic_adjustment": [False, True],
+        "case": ["mean"],
+        "little_isostatic_adjustment": [False],
         "opposite_load_on_continents": [False, True],
     },
     symlinks=True,
